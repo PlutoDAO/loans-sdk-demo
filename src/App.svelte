2570ecdbd8f0987096e34ef6e1317538eaba0e4c
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-05-02 14:39:26 -0300

    Setup svelte environment

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { Route, Router } from 'svelte-navigator';
+
+  import Home from './routes/Home.svelte';
+</script>
+
+<Router primary={false}>
+  <Route path="/" component={Home} />
+</Router>
---
+++ src/routes/__tests__/home.test.ts
@@ -0,0 +1,8 @@
+// @vitest-environment jsdom
+import { expect } from 'vitest';
+
+describe('Dummy test', () => {
+  it('should true to be true', () => {
+    expect(true).toBe(true);
+  });
+});
