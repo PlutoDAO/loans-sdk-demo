7587c0ab0658783435f3be1b07c7eb6dbc419ecf
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-06-13 12:54:06 -0300

    Format file

--- src/lib/verifyAccount/helpers/helpers.ts
+++ src/lib/verifyAccount/helpers/helpers.ts
@@ -12,10 +12,7 @@ export function mapAccountResponse(account: AccountResponse): IBorrower {
   let hasLoan = false;
 
   for (const balance of account.balances) {
-    if (
-      balance.asset_type === 'credit_alphanum4' ||
-      balance.asset_type === 'credit_alphanum12'
-    ) {
+    if (balance.asset_type === 'credit_alphanum4' || balance.asset_type === 'credit_alphanum12') {
       assets.push({ code: balance.asset_code, balance: balance.balance });
     } else {
       assets.push({ code: balance.asset_type, balance: balance.balance });
