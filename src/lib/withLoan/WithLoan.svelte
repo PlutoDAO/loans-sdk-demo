6b7171fb7f3ff8e3aaa8c3181373ae5f980de653
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-06-13 12:57:16 -0300

    Refactor withLoan components
    - WithLoan will be the component that does all logic and pass the data to the child components

--- src/lib/withLoan/WithLoan.svelte
+++ src/lib/withLoan/WithLoan.svelte
@@ -1,16 +1,90 @@
 <script>
+  import { onMount } from 'svelte';
   import SettleLoan from './SettleLoan.svelte';
   import LoanStatus from './loanStatus/LoanStatus.svelte';
   import LoanStatusCodeSnippet from './loanStatus/LoanStatusCodeSnippet.svelte';
   import { loanStatus } from './loanStatus/store';
+  import { Circle } from 'svelte-loading-spinners';
+  import { error, unsignedXdr } from './store';
+  import { getContext } from '../services/context';
+  import { borrower } from '../verifyAccount/store';
+  import WithdrawCollateral from './WithdrawCollateral.svelte';
+  import { getShortenedText } from '../utils/utils';
+  import { signedXdr } from '../services/simple-signer/store';
+  import WithdrawResult from './WithdrawResult.svelte';
+
+  const SimpleSigner = getContext('simpleSigner');
+  const loansSdk = getContext('loansSdk');
+  const server = getContext('stellar');
+  const pUSD = { code: 'pUSD', issuer: 'GBMUBDMUOSN6LMB6X2YREGIFEJRXVYOVL6EV3LGRVPQ5KPJZO5E644I4' };
+  let isStatusLoading = false;
+
+  async function handleGetLoanStatus() {
+    isStatusLoading = true;
+
+    try {
+      $loanStatus = await loansSdk.getLoanStatus(server, $borrower.publicKey);
+    } catch (e) {
+      if (e instanceof Error) {
+        const parsedError = JSON.parse(e.message);
+
+        if (parsedError.type === 'LOAN_NOT_FOUND') {
+          $error = 'Loan not found';
+        } else {
+          $error = parsedError.detail;
+        }
+      }
+    } finally {
+      isStatusLoading = false;
+    }
+  }
+
+  async function handleSettleDebt() {
+    try {
+      $unsignedXdr = '';
+
+      const asset = new loansSdk.LoanAssetRequest(false, pUSD.code, pUSD.issuer);
+      $unsignedXdr = await loansSdk.getSettleDebtIntent(server, $borrower.publicKey, asset);
+    } catch (e) {
+      throw new Error(`${e}`);
+    }
+  }
+
+  async function handleSendXdr() {
+    try {
+      const result = await loansSdk.sendWithdrawCollateral(server, $borrower.publicKey, $signedXdr);
+
+      if (result) {
+        $signedXdr = '';
+        $borrower.hasLoan = false;
+      }
+    } catch (e) {
+      throw new Error(`${e}`);
+    }
+  }
+
+  async function handleSign() {
+    SimpleSigner.sign($unsignedXdr);
+  }
+
+  onMount(async () => {
+    await handleGetLoanStatus();
+  });
 </script>
 
 <div class="container">
-  <LoanStatus />
+  {#if !isStatusLoading}
+    <LoanStatus />
+  {:else}
+    <Circle />
+  {/if}
 
   {#if $loanStatus}
     {#if $loanStatus.remainingDebt}
-      <SettleLoan />
+      <SettleLoan handleSettleDebt={handleSettleDebt}>
+        <WithdrawResult slot="result-component" xdr={getShortenedText($unsignedXdr)} handleSign={handleSign} />
+        <WithdrawCollateral slot="withdraw-component" handleSendXdr={handleSendXdr} />
+      </SettleLoan>
     {/if}
   {/if}
 
--- src/lib/withLoan/WithdrawCollateral.svelte
+++ src/lib/withLoan/WithdrawCollateral.svelte
@@ -1,32 +1,7 @@
-<script>
-  import { sendWithdrawCollateral } from 'pluto-loans-sdk';
-  import { Circle } from 'svelte-loading-spinners';
+<script lang="ts">
+  import { signedXdr } from '../services/simple-signer/store';
 
-  import { signedXdr } from '../withoutLoan/store';
-  import server from '../stellar/server';
-  import { borrower } from '../verifyAccount/store';
-
-  let isLoading = false;
-
-  async function handleSendXdr() {
-    try {
-      isLoading = true;
-      const result = await sendWithdrawCollateral(
-        server,
-        $borrower.publicKey,
-        $signedXdr,
-      );
-
-      if (result) {
-        $signedXdr = '';
-        $borrower.hasLoan = false;
-      }
-    } catch (e) {
-      throw new Error(`${e}`);
-    } finally {
-      isLoading = false;
-    }
-  }
+  export let handleSendXdr: () => void;
 </script>
 
 <label>
@@ -34,9 +9,5 @@
   <input type="text" bind:value={$signedXdr} />
 </label>
 <button on:click={handleSendXdr}>
-  {#if isLoading}
-    <Circle size="20" color="black" />
-  {:else}
-    <p>Withdraw Collateral</p>
-  {/if}
+  <p>Withdraw Collateral</p>
 </button>
