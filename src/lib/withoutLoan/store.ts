382b9cd48b58ed90dee4a8873549cec959e3d03a
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-06-14 16:59:48 -0300

    Add vars for the withoutLoan component
    - isGettingTheLoan
    - isSendingTheLoan

--- src/lib/withoutLoan/store.ts
+++ src/lib/withoutLoan/store.ts
@@ -2,3 +2,5 @@ import { writable } from 'svelte/store';
 
 export const loanXdr = writable<string>();
 export const error = writable<string>();
+export const isGettingTheLoan = writable(false);
+export const isSendingTheLoan = writable(false);
