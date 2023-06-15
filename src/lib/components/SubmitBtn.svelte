d34b5516f624cb168d14efc5e60f4e9f579d89d5
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-06-15 11:44:20 -0300

    Remove Circle component in SubmitBtn

--- src/lib/components/SubmitBtn.svelte
+++ src/lib/components/SubmitBtn.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
-  import { Circle } from 'svelte-loading-spinners';
-
   export let text: string;
   export let onClick: () => void;
   export let isDisable = false;
-  export let isLoading: boolean;
 </script>
 
 <div class="submit-container">
   <button on:click={onClick} disabled={isDisable}>
     <p>{text}</p>
   </button>
-  {#if isLoading}
-    <Circle size="20" color="white" />
-  {/if}
 </div>
 
 <style>
