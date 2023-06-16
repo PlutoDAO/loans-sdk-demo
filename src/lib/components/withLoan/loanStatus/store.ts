09dc31aa5aefd1de709223b497ab224cce77a174
venutti <lvenutti@fi.uba.ar>
2023-06-16 10:50:41 -0300

    Add initial value to loanStatus store
    To avoid undefined store errors

--- src/lib/components/withLoan/loanStatus/store.ts
+++ src/lib/components/withLoan/loanStatus/store.ts
@@ -1,4 +1,9 @@
 import type { LoanStatusResponse } from 'pluto-loans-sdk';
 import { writable } from 'svelte/store';
 
-export const loanStatus = writable<LoanStatusResponse>();
+export const loanStatus = writable<LoanStatusResponse>({
+  percentagePaid: 0,
+  remainingDebt: 0,
+  userTotalYusdcInVault: 0,
+});
+
