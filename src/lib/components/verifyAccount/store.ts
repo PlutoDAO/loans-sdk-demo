7571fbcc7b3eb4efa014d58185a0dbd7a06c2bd3
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-06-22 14:22:11 -0300

    Delete loanAmount in the VerifyAccount store

--- src/lib/components/verifyAccount/store.ts
+++ src/lib/components/verifyAccount/store.ts
@@ -2,6 +2,5 @@ import { writable } from 'svelte/store';
 
 import type IBorrower from './IBorrower';
 
-export const loanAmount = writable<number>();
 export const borrower = writable<IBorrower>();
 export const isUserConnected = writable(false);
