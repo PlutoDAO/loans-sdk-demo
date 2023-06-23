3e78218e45c0189bef48298aa54af2d03be1bfd0
venutti <lvenutti@fi.uba.ar>
2023-06-23 12:09:37 -0300

    Refactor code snippets to use CodeSnippet

---
+++ src/lib/components/withLoan/snippets/SettleDebtSnippet.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import CodeSnippet from '../../CodeSnippet.svelte';
+</script>
+
+<CodeSnippet
+  title="Settle Debt"
+  snippet={`const asset = new LoanAssetRequest(
+  'pUSD',
+  'GBMUBDMUOSN6LMB6X2YREGIFEJRXVYOVL6EV3LGRVPQ5KPJZO5E644I4',
+  false,
+);
+
+const settleDebtXDR = await getSettleDebtIntent(server, borrower, asset);
+
+// When the borrower signs the XDR, we send it
+
+const signedWithdrawCollateralXdr =
+    'AAAAAgAAAACNv+HQvu9z8arYeDQYDO5KZoPZcjtWn3QWVHFVmFgtAgAAAGQAD2qUAAAAAQAAAAE...';
+
+await sendWithdrawCollateral(server, borrower, signedWithdrawCollateralXdr);`}
+/>
---
+++ src/lib/components/withLoan/snippets/WithLoanSnippets.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import CheckLoanStatusSnippet from './CheckLoanStatusSnippet.svelte';
+  import SettleDebtSnippet from './SettleDebtSnippet.svelte';
+  import WithdrawCollateralSnippet from './WithdrawCollateralSnippet.svelte';
+</script>
+
+<section class="snippets">
+  <h2>Code Examples</h2>
+  <CheckLoanStatusSnippet />
+  <WithdrawCollateralSnippet />
+  <SettleDebtSnippet />
+</section>
+
+<style>
+  .snippets {
+    margin: 20px 0;
+  }
+</style>
