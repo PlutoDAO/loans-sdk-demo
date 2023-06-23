342b389e38e2e71aaee1471b20b12bd6f525b974
venutti <lvenutti@fi.uba.ar>
2023-06-23 17:14:51 -0300

    Create CopyBtn component to abstract functionality

---
+++ src/lib/components/CopyBtn.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import CheckIcon from '../../assets/Check.svelte';
+  import CopyIcon from '../../assets/Copy.svelte';
+  import { copyToClipboard } from '../utils/utils';
+
+  export let value: string;
+  let isCopied = false;
+
+  function handleClick() {
+    if (!value) return;
+    copyToClipboard(value);
+    isCopied = true;
+  }
+</script>
+
+<button class="copy-btn" on:click={handleClick}>
+  {#if !isCopied}
+    <CopyIcon />
+  {:else}
+    <CheckIcon />
+  {/if}
+</button>
+
+<style>
+  .copy-btn {
+    background: none;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+  }
+</style>
