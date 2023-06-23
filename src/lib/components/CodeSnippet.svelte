b1a97262484179159db5c82644ff00c1b95bc3e3
venutti <lvenutti@fi.uba.ar>
2023-06-23 12:06:03 -0300

    Create a reusable CodeSnippet component

---
+++ src/lib/components/CodeSnippet.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import Prism from 'svelte-prism';
+
+  import CopyIcon from '../../assets/Copy.svelte';
+
+  export let language: string = 'javascript';
+  export let snippet: string;
+  export let title: string = '';
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(snippet);
+  }
+</script>
+
+{#if title} <h3 class="snippet-title">{title}</h3> {/if}
+<article class="snippet-container">
+  <Prism language={language} source={snippet} />
+  <button class="copy-btn" on:click={copyToClipboard}>
+    <CopyIcon />
+  </button>
+</article>
+
+<style>
+  .snippet-title {
+    margin-top: 1rem;
+    margin-bottom: 0;
+  }
+  .snippet-container {
+    position: relative;
+    max-width: 700px;
+  }
+  :global(.snippet-container pre[class*='language-']) {
+    display: flex;
+    margin: 0;
+    padding: 0;
+  }
+  :global(pre[class*='language-']::after) {
+    content: '';
+    display: block;
+    padding: 25px;
+    background-color: inherit;
+  }
+  :global(code[class*='language-']) {
+    overflow-x: auto;
+    padding: 1rem;
+  }
+  .copy-btn {
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 10px;
+    background: none;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+  }
+</style>
