745c2cc7165617bdbbe40131a3b95284f9a41083
venutti <lvenutti@fi.uba.ar>
2023-06-23 17:09:30 -0300

    Create Stellar class in /services

---
+++ src/lib/services/stellar/Stellar.ts
@@ -0,0 +1,16 @@
+import type { ApiStellarNetwork } from 'pluto-loans-sdk';
+import { Keypair } from 'stellar-sdk';
+
+export default class Stellar {
+  static getServer(): ApiStellarNetwork {
+    return 'testnet';
+  }
+
+  static generateRandomKeypair(): Keypair {
+    return Keypair.random();
+  }
+
+  static async fundAccount(pair: Keypair) {
+    await fetch(`https://friendbot.stellar.org?addr=${encodeURIComponent(pair.publicKey())}`);
+  }
+}
