ba80dca23df93a07753625a50e66a092255e9bad
venutti <lvenutti@fi.uba.ar>
2023-06-23 17:13:03 -0300

    Replace server context to Stellar context

--- src/lib/services/context/IServicesContext.ts
+++ src/lib/services/context/IServicesContext.ts
@@ -1,11 +1,12 @@
 import type * as loansSdk from 'pluto-loans-sdk';
 
 import type SimpleSigner from '../simple-signer/SimpleSigner';
+import type Stellar from '../stellar/Stellar';
 import type toast from '../toast';
 
 export default interface IServicesContext {
   appDependencies: {
-    server: loansSdk.ApiStellarNetwork;
+    Stellar: typeof Stellar;
     SimpleSigner: typeof SimpleSigner;
     loansSdk: typeof loansSdk;
     toast: typeof toast;
