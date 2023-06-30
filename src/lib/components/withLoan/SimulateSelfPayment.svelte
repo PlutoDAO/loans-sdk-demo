1e65c27640566c9b02a678290484aa9a6fbe5237
venutti <lvenutti@fi.uba.ar>
2023-06-30 15:12:31 -0300

    Create SimulateSelfPayment component

---
+++ src/lib/components/withLoan/SimulateSelfPayment.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let show: boolean;
+  export let handleSelfPayment: () => void;
+  let isDisabled = show;
+
+  function handleClick() {
+    isDisabled = true;
+    handleSelfPayment();
+  }
+</script>
+
+{#if show}
+  <button on:click={handleClick} disabled={isDisabled}>Simulate Self Payment</button>
+{/if}
